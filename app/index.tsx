8a7ac16283a9fac50dbdbd39d7015aa3acb974f5
Ben Zeltser <benzetlser@gmail.com>
2025-04-08 13:34:09 +0300

    Minor updates

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,28 @@
 import React from 'react';
-import LoginScreens from '../screens/LoginScreens';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+// Screens
+import LoginScreen from '../screens/LoginScreen';
+import SignupScreen from '../screens/SignupScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return <LoginScreens />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="LoginScreen">
+        <Stack.Screen
+          name="LoginScreen"
+          component={LoginScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="SignupScreen"
+          component={SignupScreen}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
