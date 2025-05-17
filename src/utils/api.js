a3092fb733d948069432f3a139ba49b6c6c9ba2a
tal.gabbay <tal.gabbay@vastdata.com>
2025-05-17 15:53:06 +0300

    remove api js changes

--- src/utils/api.js
+++ src/utils/api.js
@@ -120,12 +120,6 @@ export const getReports = async (userId) => {
   const endpoint = userId ? `/reports/${userId}` : '/reports';
   return apiRequest(endpoint);
 };
-
-export const getAllReports = async => {
-  const endpoint = `/reports`;
-  return apiRequest(endpoint);
-};
-
 export const getUserProfile = async () => {
   try {
     try {
@@ -149,19 +143,3 @@ export const getUserProfile = async () => {
     throw error;
   }
 };
-
-
-export const updateReportStatus = async (reportId, newStatus) => {
-  const endpoint = `/reports/${reportId}/status`;
-  const res = await apiRequest(endpoint, {
-    method: 'PATCH',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ status: newStatus }),
-  });
-
-  if (!res.ok) throw new Error('Failed to update status');
-
-  return await res.json();
-}
